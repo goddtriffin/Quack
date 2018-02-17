f78702acf3abdab08e16d29dc70ce2ef882585df
Todd Griffin <nemagnusfrater@gmail.com>
2018-02-16 20:38:24 -0500

    Update 'graphql' dependency from v0.12.3 to v0.13.1 for now exported 'getDescription' helper function. Fill out User type and resolvers more heavily in quest to fulfill all User functionalities.

--- server/src/graphql/resolvers/user.js
+++ server/src/graphql/resolvers/user.js
@@ -1,12 +1,64 @@
+class User {
+    constructor(id, {firstName, lastName}) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.courses = [];
+    }
+
+    // addCourse () {}
+}
+
+var fakeDatabase = {};
+fakeDatabase[0] = {id: 0, firstName: "Todd", lastName: "Griffin", courses: []};
+fakeDatabase[1] = {id: 1, firstName: "Justin", lastName: "Hartman", courses: []};
+fakeDatabase[2] = {id: 2, firstName: "Theo", lastName: "Burkhart", courses: []};
+fakeDatabase[3] = {id: 3, firstName: "Mason", lastName: "Wesolek", courses: []};
+fakeDatabase[4] = {id: 4, firstName: "Tyler", lastName: "Rex", courses: []};
+
 export default {
-    User: {
-        courses: () => {},
+    // Query //
+
+    users: () => Object.values(fakeDatabase),
+
+    user: ({id}) => {
+        // make sure User with given id actually exists
+        if (!fakeDatabase[id]) {
+            // User with id doesn't exist, throw error
+            throw new Error("No user exists with id: " + id + ".");
+        }
+
+        // return User to client
+        return fakeDatabase[id];
     },
-    Query: {
-        users: () => {},
-        user: () => {},
+    
+    // Mutation //
+
+    createUser: ({input}) => {
+        // (TEMPORARY FIX) use fakeDatabase's size to create initial id
+        const id = Object.keys(fakeDatabase).length;
+
+        // create new User
+        const user = new User(id, input);
+
+        // update database with new User (potentially async task)
+        fakeDatabase[id] = user;
+
+        // return newly created User to client
+        return user;
     },
-    Mutation: {
-        addUser: () => {},
-    },
+
+    updateUser: ({id, input}) => {
+        // make sure User with given id actually exists
+        if (!fakeDatabase[id]) {
+            // User with id doesn't exist, throw error
+            throw new Error("No user exists with id: " + id + ".");
+        }
+
+        // partially/fully update User
+        Object.assign(fakeDatabase[id], input);
+        
+        // return updated User to client
+        return fakeDatabase[id];
+    }
 }
--- server/src/server.js
+++ server/src/server.js
@@ -5,30 +5,15 @@ var { buildSchema } = require('graphql');
 import types from './graphql/types';
 import rootValue from './graphql/resolvers';
 
-/*
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!';
-  }
-};
-*/
 console.log(types);
-console.log(rootValue);
+// console.log(rootValue);
 
 const schema = buildSchema(types);
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue/*: root,*/,
+  rootValue,
   graphiql: true,
 }));
 app.listen(4000);
