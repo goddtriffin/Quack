c6d0eb83b541757f088d96753315c0f73e5601b8
UbuntuVM <ubuntuVM@endor>
2018-04-01 22:16:47 -0400

    Added courseCreate functionality

--- server/src/graphql/resolvers/course.js
+++ server/src/graphql/resolvers/course.js
@@ -12,5 +12,19 @@ export default {
     course: (args, context) => {
     	argSQL[0] = {name: "name", type: TYPES.NVarChar, arg: args.name};
         return context.db.executeSQL("SELECT * FROM TestSchema.Courses where name = @name", argSQL, false);
-    }
+    },
+
+    //    MUTATIONS     //
+    courseCreate: (args, context) => {
+
+    argSQL = {};
+    argSQL[0] = {name: 'id', type: TYPES.NVarChar, arg: args.input.id};
+    argSQL[1] = {name: 'name', type: TYPES.NVarChar, arg: args.input.name};
+
+        //console.log(argSQL);
+        return context.db.executeSQL( 
+            "INSERT INTO TestSchema.Courses (id, name) OUTPUT " + 
+            "INSERTED.id, INSERTED.name " + 
+            "VALUES (@id, @name);", 
+            argSQL, false);}
 }
