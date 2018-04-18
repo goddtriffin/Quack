9409b84c81b7efe9806a04b935c8b14a2bc29513
Todd Griffin <nemagnusfrater@gmail.com>
2018-04-18 04:35:57 -0400

    Prepare testing quiz_updated subscription event.

--- server/tests/index.js
+++ server/tests/index.js
@@ -1,33 +1,3 @@
-// API CALL TESTING
-
-/*
-import fetch from "node-fetch";
-
-const endpoint = "https://quack.localtunnel.me/graphql";
-
-function sendRequest (request, callback) {
-    fetch(endpoint, {
-        method: 'POST',
-        body: JSON.stringify(request),
-        headers: { 'Content-Type': 'application/json' }
-    })
-    .then(res => res.json())
-    .then(json => callback(json));
-}
-
-// TEST
-
-const testQuery = { query: "{ users { firstName } }" }
-const testMutation = { mutation: "{ userCreate( input: { firstName: \"t1\" lastName: \"t1\" email: \"t1@purdue.edu\" }, password: \"t1\") { firstName } }" };
-
-function testCallback (json) {
-    console.log(json);
-}
-
-// sendRequest(testQuery, testCallback);
-// sendRequest(testMutation, testCallback);
-*/
-
 // VALIDATOR TESTING
 
 const v = require('../src/graphql/validators/validate');
--- server/tests/subscriptions.js
+++ server/tests/subscriptions.js
@@ -8,12 +8,47 @@
 // "Given quiz results can be viewed in real-time, a user should only be able to see their own answer and not any others for that quiz."
 // "Given quiz results can be viewed in real-time, a user should only be able to see the class results once the instructor has made it available to view."
 
+// import { request, GraphQLClient } from 'graphql-request'
+
+const { request, GraphQLClient } = require('graphql-request')
+
+const graphqlEndpoint = "http://endor-vm2.cs.purdue.edu:4000/graphql";
+const socketEndpoint = "http://endor-vm2.cs.purdue.edu:5000";
+
 // INITIAL
+
+function login (onSuccessCallbacks) {
+    // create query
+    const query = `mutation {
+        login (email: "todd@griffin", password: "bees") {
+            jwt
+        }
+    }`
+
+    // send request
+    return request(graphqlEndpoint, query)
+        .then(response => {
+            // create client with auth
+            const client = new GraphQLClient(graphqlEndpoint, {
+                headers: {
+                    Authorization: response.login.jwt,
+                },
+            });
+
+            // run success callbacks w/ authed client
+            onSuccessCallbacks.forEach(function (callback) {
+                callback(client);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
 
 // returns socket for automatic testing use
 function createSocket (type) {
     // create
-    const socket = require('socket.io-client')('http://endor-vm2.cs.purdue.edu:5000');
+    const socket = require('socket.io-client')(socketEndpoint);
 
     // handle new connection
     socket.on('connect', function () {
@@ -31,21 +66,85 @@ function createSocket (type) {
 
 // QUIZ UPDATED
 
-function subscription_quiz_updated (courseID) {
+function subscription_quiz_updated (client) {
+    // open a quiz
+    openQuiz(client);
+
     // init
     const socket = createSocket('quiz_updated');
 
-    // attach quiz updated listener
-    socket.on('quiz_updated', function () {
-
-
-        // close when done
-        socket.disconnect();
-    });
-
     // subscribe to the quiz_updated event,
     // send what course to watch
-    socket.emit('subscribe quiz_updated', courseID);
+    socket.emit('subscribe', 'quiz_updated', 123123);
+
+    // attach quiz updated listener
+    socket.on('quiz_updated', handleUpdatedQuiz);
+
+    // close a quiz
+    const boundCloseQuiz = closeQuiz.bind(null, client);
+    setTimeout(boundCloseQuiz, 1000);
+}
+
+// opens a quiz in the database
+function openQuiz (client) {
+    // create query
+    const query = `mutation {
+        quizUpdate (id: 1, input: {
+            courseID: 123123,
+            date: "04182018"
+            isOpen: true
+        }) {
+            courseID
+            date
+            isOpen
+        }
+    }`
+
+    // send request
+    client.request(query)
+        .then(response => {
+            console.log('Attempted to open quiz 1...');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+// closes a quiz in the database
+function closeQuiz (client) {
+    // create query
+    const query = `mutation {
+        quizUpdate (id: 1, input: {
+            courseID: 123123,
+            date: "04182018"
+            isOpen: false
+        }) {
+            courseID
+            date
+            isOpen
+        }
+    }`
+
+    // send request
+    client.request(query)
+        .then(response => {
+            console.log('Attempted to close quiz 1...');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+// handles updated quiz
+function handleUpdatedQuiz (quiz) {
+    // show data from updated quiz
+    console.log('quiz updated:', quiz);
+
+    // unsubscribe from the quiz_updated event
+    socket.emit('unsubscribe', 'quiz_updated');
+
+    // close when done
+    socket.disconnect();
 }
 
 // QUIZ ANSWER CREATED
@@ -56,7 +155,7 @@ function subscription_quiz_answer_created (quizID) {
 
     // attach quiz answer created listener
     socket.on('quiz_answer_created', function () {
-
+        // TODO
 
         // close when done
         socket.disconnect();
@@ -69,6 +168,4 @@ function subscription_quiz_answer_created (quizID) {
 
 // TESTALL
 
-subscription_quiz_updated(123123);
-
-subscription_quiz_answer_created(1);
+login([subscription_quiz_updated, subscription_quiz_answer_created]);
