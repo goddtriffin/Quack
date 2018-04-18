edbfc5ac4b3d93834f23989ec141ebd56129c278
Tyler Rex <rext@purdue.edu>
2018-04-18 06:35:14 -0400

    Replaced StudentID with confirmed password field

--- client/quack_nativeapp/app/components/HomeScreen/HomeScreen.js
+++ client/quack_nativeapp/app/components/HomeScreen/HomeScreen.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { View, Image, StatusBar, KeyboardAvoidingView, TouchableOpacity, Text, ScrollView, AsyncStorage, AlertIOS } from 'react-native';
 import styles from './styles';
 import { StackNavigator } from 'react-navigation';
-import { HeaderContainer, Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
-
+import { HeaderContainer, Header, Left, Body, Right, Button, Icon, Title, Item, Input } from 'native-base';
+import { Col, Row, Grid } from "react-native-easy-grid";
 import { ApolloProvider, graphql, withApollo } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
@@ -13,24 +13,28 @@ import gql from 'graphql-tag';
 class HomeScreen extends Component {
     
     static navigationOptions = {
-        header: null
+        header: null,
     };
     
 
     constructor(props) {
         super(props);
-        this.updateCourseList = this.updateCourseList.bind(this);
+        this.updateCourseList = this.addCourse.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
     }
 
     state = {
+        title: 'Courses', 
         courses: [],
         studentID:'',
         email:'',
         isLoading: true,
+        isSearching: false,
+        search: '',
+        searchResults: [],
     };
 
     componentDidMount() {
-
 
         AsyncStorage.getItem('studentID').then((token) => {
             this.setState({
@@ -44,6 +48,7 @@ class HomeScreen extends Component {
                 mutation userGetCourses($id: Int!) {
                   userGetCourses(id: $id) {
                     name
+                    id
                   }
                 }
               `,
@@ -53,20 +58,16 @@ class HomeScreen extends Component {
               console.log(data);
               courses = [];
 
-              if(data.data.userGetCourses == null) {
-                courses.push({'course' : 'No current classes'})
+              if(data.data.userGetCourses.length == 0) {
+                courses.push({course : 'Search for a course to join it.' , id: 0, key: 0})
               }
               else {
                 for(let i = 0; i < data.data.userGetCourses.length; i++) {
-                    courses.push({'course' : data.data.userGetCourses[i].name})
+                    courses.push({course : data.data.userGetCourses[i].name, id: data.data.userGetCourses[i].id, key: i})
                 }
               }
               this.setState({courses});
-            }).catch(function(error) {
-                console.log('There has been a problem with your fetch operation: ' + error.message);
-                 // ADD THIS THROW error
-                throw error;
-            });
+            })
         });
 
 
@@ -78,62 +79,81 @@ class HomeScreen extends Component {
         });
     }
 
-    updateCourseList() {
+    addCourse(id) {
 
         var title = "";
         let courses = this.state.courses;
-        AlertIOS.prompt(
-            'Enter Course ID', null, (text) => {
-
-                console.log(text);
                 
-                this.props.client.mutate({ mutation: gql`
-                    mutation userAddCourse($id: Int!, $courseID: Int!) {
-                      userAddCourse(id: $id, courseID: $courseID) {
+            this.props.client.mutate({ mutation: gql`
+                mutation userAddCourse($id: Int!, $courseID: Int!) {
+                    userAddCourse(id: $id, courseID: $courseID) {
                         name
-                      }
                     }
-                  `,
-                  variables: {
-                    id : this.state.studentID,
-                    courseID: parseInt(text)
-                   }
-                }).then( data => {
-                  courses = [];
-
-                  if(data.data.userAddCourse == null) {
-                    courses.push({'course' : 'No current classes'})
-                  }
-                  else {
-                    for(let i = 0; i < data.data.userAddCourse.length; i++) {
-                        courses.push({'course' : data.data.userAddCourse[i].name})
-                    }
-                  }
-                  this.setState({courses});
-                }).catch(function(error) {
-                    alert(error.message);
-                }); 
+                }
+            `,
+            variables: {
+                id : this.state.studentID,
+                courseID: id,
             }
-        );
+            }).then( data => {
+                courses = [];
+
+                if(data.data.userAddCourse.length == 0) {
+                    courses.push({course : 'Search for a course to join it.', id: 0, key: 1})
+                }
+                else {
+                    for(let i = 0; i < data.data.userAddCourse.length; i++) {
+                        courses.push({course : data.data.userAddCourse[i].name, id: data.data.userAddCourse[i].id, key:i})
+                    }
+                }
+                this.setState({courses})
+            })
+
+        this.setState({title:'Courses'})
+        this.setState({isSearching:false})
+        this.setState({search:''})
     }
 
-    searchCourseList() {
-        AlertIOS.prompt(
-            'Enter Course Name', null, (text) => {
-                console.log(text);
-                
+    handleSearch() {
+        if(this.state.search == ""){
+            return
+        }
+        let searchResults = [];
+        this.props.client
+            .query({
+                query: gql`
+            {
+                courses {
+                    name
+                    id
+                }
             }
-        )
-    };
+            `
+        })
+        .then(data => {
 
+        for(let i = 0; i < data.data.courses.length; i++) {
+            if(data.data.courses[i].name.toLowerCase().match(this.state.search.toLowerCase()))
+            searchResults.push({name : data.data.courses[i].name, id: data.data.courses[i].id, key: i})
+        }
+        
+        if(searchResults.length == 0) {
+            searchResults.push({name : 'Your search had no matches.', id:0, key:0})
+        }
+
+        this.setState({isSearching:true})
+        this.setState({title:'Search Results'})
+        this.setState({searchResults})
+        });
+    }
+
+    reset() {
+        this.setState({title:'Courses'})
+        this.setState({isSearching:false})
+        this.setState({search:''})
+    }
 
     render() {
-
-
-
-        let AddCourseButton = <Text/>;
-        let CourseDetails = <CourseDetails/>;
-        AddCourseButton = <Text style={styles.addCourseText}>+ Add course</Text>
 
         if(this.state.isLoading) {
             return(<View><Text>Loading...</Text></View>);
@@ -143,64 +163,57 @@ class HomeScreen extends Component {
 
         return (
             <View style={styles.container}>
-                <Header style={styles.headerColor}>
-                    <Left>
-                        
-                        <Button transparent onPress={() => this.props.navigation.navigate('Feedback')}>
-                            <Icon style={{color: 'white', paddingTop: 25}} name='settings'/>
-                        </Button>
-                    </Left>
-                    <Body></Body>
-                    <Right>
-                        <Button transparent onPress={() => this.updateCourseList()}>
-                            <Icon style={{color: 'white', paddingTop: 25}} name='search'/>
-                        </Button>
-                        <Text style={styles.headerText}>Course search</Text>
-                    </Right>
+                <Header searchBar rounded style={styles.header}>
+                    <Item>
+                        <Icon name="ios-search"/>
+                            <Input placeholder="Search Courses"
+                            onChangeText={(search) => this.setState({search})}
+                            onSubmitEditing={() => this.handleSearch()} 
+                            autoCorrect={false}
+                            autoCapitalize={false}
+                            returnKeyType="search"
+                            value={this.state.search}
+                            />
+                        <Icon name="close" onPress={()=> this.reset()}/>
+                    </Item>
+                    <Button transparent onPress={() => this.props.navigation.navigate('Feedback')}>
+                        <Icon style={{color: 'white'}} name='settings'/>
+                    </Button>
                 </Header>
-{/*
-                <Header>
-                    <Left>
-                        <Button large transparent>
-                        <Icon name='menu'/>
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Title>Classes</Title>
-                    </Body>
-                    <Right>
-                        <Button large transparent>
-                        <Icon name='ios-search'/>
-                        </Button>
-                        <Button large transparent onPress={() => this.updateCourseList()}>
-                        <Icon name='add'/>
-                        </Button>
-                    </Right>
-                </Header>
-*/}
-                <StatusBar
-                    barStyle="default"
-                />
                 
                 <View style={styles.header}>
                     <Text style={styles.bigTitle}>
-                        Classes
+                        {this.state.title}
                     </Text>
                 </View>
 
                 <View style={styles.courseListView}>
                     <ScrollView style={styles.courseList}>
-                        {
-                            this.state.courses.map(({course}) => {
+                        { (this.state.isSearching == false) ?
+                            this.state.courses.map(({course, id}) => {
                                     return (<View>
-                                    <TouchableOpacity style={styles.courseListRow} onPress={() => this.props.navigation.navigate('Grades', {courses:course})}>
+                                    <TouchableOpacity style={styles.courseListRow} onPress={() => this.props.navigation.navigate('Grades', {course, id})}>
                                         <Text style={styles.courseListText}>{course}</Text>
                                     </TouchableOpacity>
                                 </View>);
                                 }
+                            )
+                            : this.state.searchResults.map(({name, id}) => {
+                                    return (<View>
+                                        <Grid>
+                                            <Col size={85}>
+                                                <Text style={styles.courseListText}>{name}</Text>
+                                            </Col>
+                                            <Col size={15}>
+                                                <TouchableOpacity style={styles.courseListRow} onPress={() => this.addCourse(id)}>
+                                                    <Icon style={styles.addButton} name='add'/>
+                                                </TouchableOpacity>
+                                            </Col>
+                                        </Grid>
+                                </View>);
+                                }
                             ) 
                         }
-                    
                     </ScrollView>
                 </View>
                 
