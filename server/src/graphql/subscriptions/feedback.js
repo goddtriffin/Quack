87eb366d44bdfbff1c7035cff513725a4f91ecfe
Todd Griffin <nemagnusfrater@gmail.com>
2018-04-20 09:14:11 -0400

    Refactor 'feedback-created' 'GraphQL Subscription's to not worry about any item's ID.

--- server/src/graphql/subscriptions/feedback.js
+++ server/src/graphql/subscriptions/feedback.js
@@ -1,50 +1,33 @@
 // containers
-const feedbackCreationSubscribers = {}; //  key:value  |  feedbackID:[socket]
+const feedbackCreationSubscribers = []; //  [socket]
 
-// subscribes a socket to feedback creations from a specific course
-function subscribeToFeedbackCreation (socket, feedbackID) {
-    // if feedbackID isn't subscribed to yet,
-    // create it as a key and pair it with an empty socket list
-    if (!feedbackCreationSubscribers.hasOwnProperty(feedbackID)) {
-            feedbackCreationSubscribers[feedbackID] = [];
-    }
-
-    // push the socket into the subscriber list attached to the specific feedbackID
-    feedbackCreationSubscribers[feedbackID].push(socket);
+// subscribes a socket to feedback creations
+function subscribeToFeedbackCreation (socket) {
+    // push the socket into the subscriber list
+    feedbackCreationSubscribers.push(socket);
 }
 
-// unsubscribes a socket to feedback creations from a specific course
-function unsubscribeToFeedbackCreation (socket, feedbackID) {
-    // if feedbackID doesn't exist in feedback update subscriber list,
-    // error, don't do anything
-    if (!feedbackCreationSubscribers.hasOwnProperty(feedbackID)) {
-            console.error('Feedback creation (un)subscriber error: feedbackID', feedbackID, 'doesn\'t exist.');
-            return;
-    }
-
-    // get index of socket within feedbackID subscriber list
-    const index = feedbackCreationSubscribers[feedbackID].indexOf(socket);
+// unsubscribes a socket to feedback creations
+function unsubscribeToFeedbackCreation (socket) {
+    // get index of socket within subscriber list
+    const index = feedbackCreationSubscribers.indexOf(socket);
 
     // check if socket exists within feedbackID subscriber list
     if (index === -1) {
-            console.error('Feedback creation (un)subscriber error: socket', socket.id, 'isn\'t subscribed to ', feedbackID,'.');
-            return;
+	    console.error('Feedback creation (un)subscriber error: socket', socket.id, 'isn\'t subscribed to feedback creations.');
+	    return;
     }
 
-    // everything gucci, remove socket from feedbackID subscriber list
-    feedbackCreationSubscribers[feedbackID].splice(index, 1);
+    // everything gucci, remove socket from subscriber list
+    feedbackCreationSubscribers.splice(index, 1);
 }
 
-// send newly created feedback information to all sockets
-// that are subscribed to the corresponding feedbackID
-function sendFeedbackCreationEvent (feedbackID, feedback) {
-    // if feedbackID is being subscribed to,
-    if (feedbackCreationSubscribers.hasOwnProperty(feedbackID)) {
-            // send updated feedback info to socket
-            feedbackCreationSubscribers[feedbackID].forEach(function (socket) {
-                    socket.emit('quiz_feedback_created', feedback);
-            });
-    }
+// send newly created feedback information to all sockets that are subscribed
+function sendFeedbackCreationEvent (feedback) {
+    // send updated feedback info to socket
+    feedbackCreationSubscribers.forEach(function (socket) {
+		socket.emit('quiz_feedback_created', feedback);
+	});
 }
 
 export default {
