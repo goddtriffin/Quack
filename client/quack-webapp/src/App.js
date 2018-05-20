748c0957fb6197c42e04fa7b966be0b46bc995c0
Todd Griffin <nemagnusfrater@gmail.com>
2018-05-20 15:38:05 -0700

    Clean up webapp root src directory.

--- client/quack-webapp/src/App.js
+++ client/quack-webapp/src/App.js
@@ -13,80 +13,77 @@ import Login from './components/Login/login';
 import { AUTH_TOKEN } from './constants'
 import { Grid, Col, Row } from '../node_modules/react-bootstrap'
 import {
-  Route,
-  Switch,
-  Redirect,
-  withRouter
+	Route,
+	Switch,
+	Redirect,
+	withRouter
 } from 'react-router-dom'
 
-
 class App extends Component {
+	state = {
+		userID: "",
+		userEmail: "",
+		loggedIn: false
+	}
 
-  state = {
-    userID: "",
-    userEmail: "",
-    loggedIn: false
-  }
+	render() {
 
-  render() {
+		const sidebar = <SidebarContent params={this.props.params}/>
 
-    const sidebar = <SidebarContent params={this.props.params}/>
+		document.body.style.overflow = "hidden"
+		const sidebarProps = {
+			sidebar: sidebar,
+			docked: true,
+			shadow: false,
+			touchHandleWidth: 20,
+			dragToggleDistance: 30,
+			touch: false,
+			transitions: false,
+			pullRight: false,
+			open: true
+		}
 
-    document.body.style.overflow = "hidden"
-    const sidebarProps = {
-      sidebar: sidebar,
-      docked: true,
-      shadow: false,
-      touchHandleWidth: 20,
-      dragToggleDistance: 30,
-      touch: false,
-      transitions: false,
-      pullRight: false,
-      open: true,
-    }
+		const CourseParent = ({ match, location }) => {
+			return(<Course courseID={match.params.courseID} location={location}/>);
+		}
 
-    const CourseParent = ({ match, location }) => {
-      return(<Course courseID={match.params.courseID} location={location}/>);
-    }
-    const SidebarParent = (props) => {
-      return(<Sidebar {...sidebarProps} className="Sidebar" location={props.location}/>);
-    }
-    const authToken = localStorage.getItem(AUTH_TOKEN);
-    if(!authToken) {
-      return (
-          <Login/>
-      );
-    }
+		const SidebarParent = (props) => {
+			return(<Sidebar {...sidebarProps} className="Sidebar" location={props.location}/>);
+		}
+
+		const authToken = localStorage.getItem(AUTH_TOKEN);
+		if (!authToken) {
+			return (<Login/>);
+		}
     
+		return (
+			<div>
+			<Grid fluid={true}>
+				<Row >
+					<Col md={1} style={{height: '100vh', width: '175px', overflowY: 'scroll'}}>
+						<div>
+							<Route render={({ location }) => (
+							<Sidebar {...sidebarProps} className="Sidebar" location={location}/>
+							)}/>
+						</div>
+					</Col>
 
-    return (
-        <div>
-          <Grid fluid={true}>
-            <Row >
-              <Col md={1} style={{height: '100vh', width: '175px', overflowY: 'scroll'}}>
-                {/* <Sidebar {...sidebarProps} className="Sidebar" location={location}/> */}
-                  <div>
-                    <Route render={({ location }) => (
-                      <Sidebar {...sidebarProps} className="Sidebar" location={location}/>
-                    )}/>
-                  </div>
-              </Col>
-              <Col md={10}>
-                  <Switch>
-                    <Route exact path="/" component={Blank}/>
-                    <Route path="/course/:courseID/new" component={NewQuiz}/> 
-                    <Route path="/course/:courseID/view/:quizID" component={QuizResults} />
-                    <Route path="/course/:courseID/quiz/:quizID" component={EditQuiz} />
-                    <Route path="/course/:courseID/start/:quizID" component={StartQuiz} />
-                    <Route path="/course/:courseID" component={CourseParent}/>
-                    <Redirect to="/" />
-                  </Switch>
-              </Col>
-            </Row>
-          </Grid>
-        </div>
-    );
-  }
+					<Col md={10}>
+						<Switch>
+							<Route exact path="/" component={Blank}/>
+							<Route path="/course/:courseID/new" component={NewQuiz}/> 
+							<Route path="/course/:courseID/view/:quizID" component={QuizResults} />
+							<Route path="/course/:courseID/quiz/:quizID" component={EditQuiz} />
+							<Route path="/course/:courseID/start/:quizID" component={StartQuiz} />
+							<Route path="/course/:courseID" component={CourseParent}/>
+							<Redirect to="/" />
+						</Switch>
+					</Col>
+				</Row>
+			</Grid>
+			</div>
+		);
+  	}
 }
 
 export default withRouter(App);
